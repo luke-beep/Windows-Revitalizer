7660ae3ea56833b64a51c92ce3b726d0fdf6aef7
LukeHjo <lukehjo@gmail.com>
2023-06-10 02:36:17 +0200

    Delete downloadSetTimerResolutionService.bat

--- Resources/downloadSetTimerResolutionService.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set "outputFile=%SystemDrive%\WindowsRevitalizer\SetTimerResolutionService.exe"
-curl -g -L -# -o "%outputFile%" "https://github.com/auraside/HoneCtrl/raw/main/Files/SetTimerResolutionService.exe"

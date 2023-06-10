0afd2ffdaa19345acd42b021322f7af54b6136fe
LukeHjo <lukehjo@gmail.com>
2023-06-10 17:55:24 +0200

    Add files via upload

---
+++ Interfaces/ILogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsRevitalizer.Interfaces
+{
+    internal interface ILogger
+    {
+        void Log(string message);
+        void ClearLogFile();
+    }
+}

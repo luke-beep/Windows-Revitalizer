5720dbb403593a882bedb3a9d8be081f82fade57
LukeHjo <lukehjo@gmail.com>
2023-06-10 02:36:31 +0200

    Create downloadSetTimerResolutionService.bat

---
+++ Resources/downloadSetTimerResolutionService.bat
@@ -0,0 +1,3 @@
+@echo off
+set "outputFile=%SystemDrive%\WindowsRevitalizer\SetTimerResolutionService.exe"
+curl -g -L -# -o "%outputFile%" "https://github.com/auraside/HoneCtrl/raw/main/Files/SetTimerResolutionService.exe"
